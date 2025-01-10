44e0a31ed763823799269107ef70e35ddf10b7ed
OGisidore <isidoreogoulodo43@gmail.com>
2025-01-10 01:43:43 +0100

    refactor CollectionPage to fetch collection details; update state management and display collection title

--- app/collections/[id]/page.tsx
+++ app/collections/[id]/page.tsx
@@ -2,11 +2,12 @@
 import React from 'react';
 import { useParams } from 'next/navigation';
 import Link from 'next/link';
-import { getCollection, getCollectionPhotos } from '@/lib/connect-toApii';
+import { getCollection } from '@/lib/connect-toApii';
 
 const CollectionPage: React.FC = () => {
     const { id } = useParams() as { id: string };
-    const [photos, setPhotos] = React.useState<any[]>([]);
+    // const [photos, setPhotos] = React.useState<any[]>([]);
+    const [collection, setCollections] = React.useState<any>();
     const [totalPhotos, setTotalPhotos] = React.useState<number>(0);
     React.useEffect(() => {
         fetchCollection();
@@ -14,11 +15,11 @@ const CollectionPage: React.FC = () => {
 
     const fetchCollection = async () => {
         try {
-            const response: any = await getCollectionPhotos(id);
+            const response: any = await getCollection(id);
             console.log(response);
             
-            setTotalPhotos(response.total);
-            setPhotos(response.data);
+            setCollections(response);
+            setTotalPhotos(response.total_photos);
             console.log(response);
         } catch (error) {
             console.error('Error fetching collection:', error);
@@ -29,7 +30,7 @@ const CollectionPage: React.FC = () => {
     return (
         <div className='flex flex-col items-center'>
             <div className="flex items-center justify-center max-w-[450] space-y-4 flex-col my-8 p-4"> 
-                <h1 className='text-gradient font-semibold text-xl md:text-4xl' >Collections</h1>
+                <h1 className='text-gradient font-semibold text-xl md:text-4xl' > {collection?.title} </h1>
                 <p className="clas text-center font-light">
                     {totalPhotos} photos
 
